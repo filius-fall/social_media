ec7bb90a8271b97ed8f42ff910abb9cc36a28b70
filius-fall <sreeram596@gmail.com>
2020-07-04 15:39:01 +0530

    added models

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
 
 
 app = Flask(__name__, instance_relative_config=True)
 
 app.config.from_pyfile("config.py")
+app.config.from_object("config")
 
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
