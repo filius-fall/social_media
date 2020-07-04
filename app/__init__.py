ea127ffae5874c42378b74f5058abdf392bfc62d
filius-fall <sreeram596@gmail.com>
2020-07-04 19:50:10 +0530

    added database

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 
-
-app = Flask(__name__, instance_relative_config=True)
-
-app.config.from_pyfile("config.py")
-app.config.from_object("config")
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 
 
 db = SQLAlchemy(app)
--- app/models.py
+++ app/models.py
@@ -3,25 +3,21 @@ from datetime import datetime
 
 
 class User(db.Model):
-    __tabelname__ = "user"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    posts = db.relationship("Post", backref="author", lazy="dynamic")
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship("Post", backref="author", lazy=True)
 
     def __repr__(self):
-        return "<User {}>".format(self.username)
+        return f"User('{self.username}', '{self.email}')"
 
 
 class Post(db.Model):
-    __tabelname__ = "post"
-
     id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    body = db.Column(db.String(144))
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
     def __repr__(self):
-        return "<Post {}>".format(self.body)
-
+        return f"Post('{self.content}', '{self.timestamp}')"
